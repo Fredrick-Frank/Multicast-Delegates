f45c8d61f510850f239f1abae7aeb56fd203a963
Fredrick-Frank <34188600+Fredrick-Frank@users.noreply.github.com>
2022-03-02 12:55:25 +0100

    Add files via upload
    Multicast Delegates is a kind of delegates that points multiple methods

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MulticastDelegateApp
+{
+    public delegate void MyDelegate(string msg); //MyDelegate is a delegate Name
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyDelegate del1 = ClassA.MethodA;
+            MyDelegate del2 = ClassB.MethodB;
+
+            MyDelegate del = del1 + del2; //combines del1 + del2
+            Console.WriteLine("After del1 + del2");
+            del("Hello , World");
+
+            MyDelegate del3 = (string msg) => Console.WriteLine("Called lambda expression: "  + msg);
+            del += del3; //combines del1 + del2 + del3
+            Console.WriteLine("After del1 + del2 + del3");
+            del("Hello World");
+
+            del = del - del2; //removes del2
+            Console.WriteLine("After del - del2");
+            del("Hello World");
+
+            del -= del1; //removes del1
+            Console.WriteLine("After del1 - del1");
+            del("Hello World");
+        }
+        public class ClassA
+        {
+            public static void MethodA(string message)
+            {
+                Console.WriteLine("Called ClassA.MethodA() with parameter: " + message);
+            }
+        }
+        public class ClassB
+        {
+            public static void MethodB(string message)
+            {
+                Console.WriteLine("Called ClassB.MethodB() with parameter: " + message);
+            }
+        }
+    }
+}
